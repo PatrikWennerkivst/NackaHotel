1cc3775c6b70c3c7033ae937fa3980c9a37f774e
Maria J Kirkou <mariajamalaki@hotmail.com>
2025-05-15 22:46:35 +0200

    Ny branch med validering

--- src/main/java/com/example/nackahotel/Controller/CustomerController.java
+++ src/main/java/com/example/nackahotel/Controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.example.nackahotel.Controller;
 
+
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
+
 
 
 
--- src/main/java/com/example/nackahotel/DTO/CustomerDTO.java
+++ src/main/java/com/example/nackahotel/DTO/CustomerDTO.java
@@ -1,10 +1,24 @@
 package com.example.nackahotel.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
 
-    
+    @NotEmpty(message = "please write your name")
+    @Size(min = 3,message = "at least 3 characters ")
+    private String name;
+
+    @NotEmpty(message = "Email required")
+    @Email(message = "Invalid email address")
+    private String email;
+
+    @NotEmpty(message = "Phone number required")
+    @Pattern(regexp = "^\\d{10}$", message = "Phone number must be exactly 10 digits")
+    private String phoneNumber;
 
 }
