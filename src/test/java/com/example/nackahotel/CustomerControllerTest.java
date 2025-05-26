7410ee09620ba22185263b91e0b6bad62b0f1707
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-05-27 00:59:20 +0200

    Gurnden för 2 tester klara

--- src/test/java/com/example/nackahotel/CustomerControllerTest.java
+++ src/test/java/com/example/nackahotel/CustomerControllerTest.java
@@ -1,7 +1,6 @@
 package com.example.nackahotel;
 
-import com.example.nackahotel.Repository.CustomerRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.nackahotel.Repository.CustomerRepository;;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
