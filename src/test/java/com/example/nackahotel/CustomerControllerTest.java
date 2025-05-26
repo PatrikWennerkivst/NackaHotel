e7126608cca579fb2e4fa5b1a93ca1ac673c6cf7
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-05-27 00:05:23 +0200

    Gurnden för 2 tester klara

--- src/test/java/com/example/nackahotel/CustomerControllerTest.java
+++ src/test/java/com/example/nackahotel/CustomerControllerTest.java
@@ -1,25 +1,24 @@
 package com.example.nackahotel;
 
-import com.example.nackahotel.DTO.CreateCustomerDTO;
 import com.example.nackahotel.Repository.CustomerRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-test.properties") // för att
+@TestPropertySource(locations = "classpath:application-test.properties")
 @Transactional // gör så att alla ändringar återställs efter körning
 public class CustomerControllerTest {
 
@@ -29,37 +28,28 @@ public class CustomerControllerTest {
     @Autowired
     private CustomerRepository customerRepository;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Test
     public void testAddCustomer() throws Exception {
 
         long initialCount = customerRepository.count();
 
-        CreateCustomerDTO customerData = new CreateCustomerDTO(
-                "Testing",
-                "Testingson",
-                "1234567890",
-                "0733355777"
-        );
-
-        // gör om DTO till JSON
-        String customerJson = objectMapper.writeValueAsString(customerData);
-
         mockMvc.perform(post("/customers/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(customerJson))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(containsString("\"firstName\":\"Testing\"")))
-                .andExpect(content().string(containsString("\"lastName\":\"Testingson\"")))
-                .andExpect(content().string(containsString("\"socialSecurityNumber\":\"1234567890\"")))
-                .andExpect(content().string(containsString("\"phoneNumber\":\"0733355777\"")))
-                .andExpect(content().string(containsString("\"id\":"))) // Verifierar att ID finns
-                .andExpect(content().string(containsString("\"bookings\":[]"))); // Tom bookings-array
+                        .param("firstName", "Testing")
+                        .param("lastName", "Testingson")
+                        .param("socialSecurityNumber", "1234567890")
+                        .param("phoneNumber", "0733355777"))
+                .andExpect(status().isOk());
 
         // kollar så att en customer har lagts till i db
         assertEquals(initialCount + 1, customerRepository.count());
     }
+
+    @Test
+    public void testGetAllCustomers() throws Exception {
+        mockMvc.perform(get("/customers"))
+                .andExpect(status().isOk());
+
+    }
+
+
 }
