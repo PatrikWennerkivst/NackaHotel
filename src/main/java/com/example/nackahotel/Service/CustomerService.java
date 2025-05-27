450806640b1684ae3e881670af6925692fda05f2
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-05-27 21:04:10 +0200

    daily stand up

--- src/main/java/com/example/nackahotel/Service/CustomerService.java
+++ src/main/java/com/example/nackahotel/Service/CustomerService.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class CustomerService {
+public class CustomerService { 
 
     private final CustomerRepository customerRepository;
     private final Mapper mapper;
